c724cbdd8ce22bdc80380fd7a77e392a0434b852
Wojciech Mierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-12-10 14:30:03 +0100

    change

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -4,7 +4,7 @@ export default function MealDetailPage({ params }) {
     return (
         <div style={{ textAlign: "center" }}>
             <h1>{params.someName.toUpperCase()}</h1>
-            <p>Let's try some {params.someName} !!!</p>
+            <p>Lets try some {params.someName}</p> 
             <div style={{
                 display: "flex",
                 flexDirection: "column",
