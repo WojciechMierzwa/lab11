8c4c591813717b06f2ececb53ab827fdbc287f4f
Wojciech Mierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-12-10 20:03:45 +0100

    No next config ?? Was removed ealier

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', 
+    basePath: '/lab11',
+    images: {
+      unoptimized: true, 
+    },
+  };
+  
+  module.exports = nextConfig;
