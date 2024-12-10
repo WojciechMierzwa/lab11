f53e8c3a167e7897016a4e9c739e165266b1b6ed
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2024-12-10 14:36:48 +0100

    Update page.js

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -1,21 +1,40 @@
 import ShareMeal from "../share/page";
 
+// Define the dynamic paths for static generation
+export async function generateStaticParams() {
+    const mealNames = [
+        "burger",
+        "curry",
+        "dumplings",
+        "macncheese",
+        "pizza",
+        "schnitzel",
+        "tomato-salad",
+    ];
+
+    return mealNames.map((someName) => ({
+        someName,
+    }));
+}
+
 export default function MealDetailPage({ params }) {
     return (
         <div style={{ textAlign: "center" }}>
             <h1>{params.someName.toUpperCase()}</h1>
-            <p>Lets try some {params.someName}</p> 
-            <div style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                margin: "20px 0"
-            }}>
-                <img 
-                    src={`/images/${params.someName}.jpg`} 
-                    style={{ width: "300px", height: "auto" }} 
-                    alt={`Image of ${params.someName}`} 
+            <p>Lets try some {params.someName}</p>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    margin: "20px 0",
+                }}
+            >
+                <img
+                    src={`/images/${params.someName}.jpg`}
+                    style={{ width: "300px", height: "auto" }}
+                    alt={`Image of ${params.someName}`}
                 />
                 <ShareMeal />
             </div>
